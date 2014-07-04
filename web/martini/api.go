de98c5d12cee400c5b1e295da7b4f8f8949b4b71
thewawar <thewawar@gmail.com>
2014-07-04 10:28:45 +0800

    Move file

--- web/martini/api.go
+++ web/martini/api.go
@@ -1,6 +1,10 @@
 package main
 
+import (
+	"time"
+)
 
 func index() (int, string) {
-	return 500, "This is index."
+	time.Sleep(200 * time.Millisecond)
+	return 200, "This is index."
 }
--- web/martini/gateway.go
+++ web/martini/gateway.go
@@ -2,11 +2,22 @@ package main
 
 // Created at: [2014-06-28 13:04]
 
+/* ============================================================================
+   Requirements
+   ------------
+   + https support (https://)
+   + secure websocket (wss://)
+   + Performance test >> http://ziutek.github.io/web_bench/
+      - siege -c 200 -t 20s http://ADDRESS:PORT/Hello/100
+ * ==========================================================================*/
+
+
 import (
 	"log"
 	"net/http"
+	"runtime"
 	"github.com/go-martini/martini"
- 	"github.com/martini-contrib/auth"
+// 	"github.com/martini-contrib/auth"
 )
 
 
@@ -15,10 +26,12 @@ const AuthUser = "user"
 const AuthPasswd = "passwd"
 
 func init() {
+	runtime.GOMAXPROCS(4)
+	
 	m = martini.New()
 	m.Use(martini.Recovery())
 	m.Use(martini.Logger())
-	m.Use(auth.Basic(AuthUser, AuthPasswd))
+	// m.Use(auth.Basic(AuthUser, AuthPasswd))
 	
 	r := martini.NewRouter()
 	r.Get(`/`, index)
@@ -29,8 +42,5 @@ func init() {
 
 func main() {
 	log.Println("Listening on :9001")
-	err := http.ListenAndServeTLS(":9001", "cert.pem", "key.pem", m);
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServeTLS(":9001", "res/cert.pem", "res/key.pem", m))
 }
